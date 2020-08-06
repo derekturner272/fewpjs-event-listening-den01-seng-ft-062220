23fbaebff7a091f01fffbfcaaa81390733819aa7
Derek Turner <turner.derek.b@gmail.com>
2020-08-06 12:51:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+const input = document.getElementById('input')
+input.addEventListener('click', function(event){
+  alert('I was clicked!!');
+});
 }
+
+
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', (event) => {
+//   alert('I was clicked!!');
+// });
